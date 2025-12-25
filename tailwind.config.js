49fd2284d4b19a1b13b6a1b387b4d3cb325218f9
Rida Saghir Qureshi <159681694+rsq-codequeen@users.noreply.github.com>
2025-12-25 08:34:25 +0500

    Update Tailwind CSS configuration with theme and plugins

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,47 @@
-// postcss.config.mjs
-export default {
-  plugins: {
-    '@tailwindcss/postcss': {},
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+
+  content: [
+    './*.html',
+  ],
+
+  theme: {
+    extend: {
+      colors: {
+        'primary': '#057B99',  
+        'secondary': '#FFC977', 
+        'success': '#FFF7D8',  
+        'danger': '#CD1010',   
+        'neutral-light': '#029A9A1F', 
+        'neutral-dark': '#1f2937', 
+        'dark-primary':'#029A9A',
+      },
+      
+     boxShadow: {
+        'around': '0 0 15px 5px rgba(0, 0, 0, 0.1)',
+      },
+      fontFamily: {
+      
+        sans: ['Inter', 'sans-serif'], 
+      },
+
+      keyframes: {
+        wiggle: {
+          '0%, 100%': { transform: 'rotate(-3deg)' },
+          '50%': { transform: 'rotate(3deg)' },
+        }
+      },
+      animation: {
+        wiggle: 'wiggle 1s ease-in-out infinite',
+      },
+    },
+
+    
   },
-};
+
+ plugins: [
+    require('@tailwindcss/typography'), 
+    require('@tailwindcss/forms'),      
+  ],
+  
+}
